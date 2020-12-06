987391f6742bef8c7c1c7370918c69715071f618
Erik Zemčík <zemcik.erik@gmail.com>
2020-12-06 22:13:36 +0100

    Created the project

---
+++ src/main/java/sk/zemco/appslabspringboot/AppsLabSpringBootApplication.java
@@ -0,0 +1,13 @@
+package sk.zemco.appslabspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppsLabSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppsLabSpringBootApplication.class, args);
+    }
+
+}
